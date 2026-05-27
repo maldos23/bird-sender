431fd9fe3e64e660c4da515d8c44be059509d663
maldos23 <42344766+maldos23@users.noreply.github.com>
2026-05-26 21:03:45 -0600

    🔧 Fix Cypress config file format

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    supportFile: false,
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    video: false,
+    screenshotOnRunFailure: false
+  }
+});
